0060d630922d18cb42c6fe1faeed9324247b2457
k-nasa <htilcs1115@gmail.com>
2019-10-07 15:47:29 +0900

    transrate 1-2 examples

--- examples/01_02_why_async/src/lib.rs
+++ examples/01_02_why_async/src/lib.rs
@@ -15,11 +15,11 @@ fn download(_url: &str) {
 #[test]
 // ANCHOR: get_two_sites
 fn get_two_sites() {
-    // Spawn two threads to do work.
+    // 作業を行うために2つのスレッドを作成します。
     let thread_one = thread::spawn(|| download("https:://www.foo.com"));
     let thread_two = thread::spawn(|| download("https:://www.bar.com"));
 
-    // Wait for both threads to complete.
+    // 両方のスレッドが完了するまで待ちます。
     thread_one.join().expect("thread one panicked");
     thread_two.join().expect("thread two panicked");
 }
@@ -31,12 +31,12 @@ async fn download_async(_url: &str) {
 
 // ANCHOR: get_two_sites_async
 async fn get_two_sites_async() {
-    // Create a two different "futures" which, when run to completion,
-    // will asynchronously download the webpages.
+    // 2つの異なるfutureを作成します。これらは、完了するまで実行すると
+    // 非同期でWebページをダウンロードします。
     let future_one = download_async("https:://www.foo.com");
     let future_two = download_async("https:://www.bar.com");
 
-    // Run both futures to completion at the same time.
+    // 両方のfutureを同時に完了するまで実行します。
     join!(future_one, future_two);
 }
 // ANCHOR_END: get_two_sites_async
